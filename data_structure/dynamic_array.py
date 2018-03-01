7f78b5690d02f5154b6b7a5ecf08125294c1f094
mhjeon <mhjeon@modernlab.io>
2018-03-01 18:05:21 +0900

    Init DynamicArray ADT

---
+++ data_structure/dynamic_array.py
@@ -0,0 +1,27 @@
+import ctypes
+
+# Python implementation of dynamic arrays
+
+__author__ = 'Myong-Hoon Jeon'
+
+
+class DynamicArray(object):
+    """
+    """
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self):
+        pass
+
+    def append(self):
+        pass
+
+    def _resize(self):
+        pass
+
+    def _make_array(self):
+        pass
