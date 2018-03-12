2353d6260de8bc534cc7ff08f95ac35dac3cfe7d
mhjeon <mhjeon@modernlab.io>
2018-03-12 11:01:22 +0900

    Add solution to mock test: find nearest two points

--- algorithm/goormedu/2016_mock_test_from_previous_exams/1-find-nearest-two-points.py
+++ algorithm/goormedu/2016_mock_test_from_previous_exams/1-find-nearest-two-points.py
@@ -1,3 +1,4 @@
+"""
 # -*- coding: utf-8 -*-
 # UTF-8 encoding when using korean
 
@@ -26,3 +27,24 @@ if num_list[local_min['i']] < num_list[local_min['j']]:
     print(num_list[local_min['i']], num_list[local_min['j']])
 else:
     print(num_list[local_min['j']], num_list[local_min['i']])
+"""
+
+if __name__ == '__main__':
+    num = int(input())
+    n_list = [int(x) for x in input().split()]
+
+    sorted_list = sorted(n_list)
+
+    # 첫번째 케이스를 미리 temp min값에 넣어두자
+    temp_min = sorted_list[1] - sorted_list[0]
+    sum = sorted_list[1] + sorted_list[0]
+    num1, num2 = sorted_list[0], sorted_list[1]
+
+    for i in range(len(sorted_list) - 1):
+        local_min = sorted_list[i+1] - sorted_list[i]
+        # 이미 정렬되어 있으므로 두개의 합이 최소가 되는지 비교할 필요가 없다.
+        if (sorted_list[i+1] - sorted_list[i] < temp_min):
+            temp_min = local_min
+            num1, num2 = sorted_list[i], sorted_list[i+1]
+
+    print(num1, num2)
